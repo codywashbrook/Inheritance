7b5503506a3356747e18b7f6ad005cfc6d5fc7b8
Cody Washbrook <codywashbrook@hotmail.com>
2023-01-16 13:47:42 -0400

    Initial commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
